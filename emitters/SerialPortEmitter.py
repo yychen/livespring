a8f830267bd6cabfa94d4be37e47d38d88ecbb3d
Tom Chen <tom@ifp.io>
2019-09-13 15:19:23 +0800

    Add led-pattern-4 for SerialPortEmitter and support for triggering signals when releasing keys

--- emitters/SerialPortEmitter.py
+++ emitters/SerialPortEmitter.py
@@ -10,6 +10,7 @@ import glob
 class SerialPortEmitter(Emitter):
     def __init__(self):
         self._targets = []
+        self.last = ''
 
     def connect(self):
         current_names = [ser.name for ser in self._targets]
@@ -23,23 +24,33 @@ class SerialPortEmitter(Emitter):
 
     def emit(self, event):
         data = event.get('data')
+        signal = None
         if data.get('type') == 'signal':
-            signal = None
             if data.get('name') == 'led-pattern-1':
                 signal = b'\x07'
             elif data.get('name') == 'led-pattern-2':
                 signal = b'\x01'
             elif data.get('name') == 'led-pattern-3':
                 signal = b'\x08'
+            elif data.get('name') == 'led-pattern-4':
+                signal = b'\x04'
             elif data.get('name') == 'led-clear':
                 signal = b'\x10'
             elif data.get('name') == 'led-text':
                 signal = b'\x02'
 
             if signal:
-                for ser in self._targets:
-                    try:
-                        ser.write(signal)
-                        ser.flush()
-                    except OSError:
-                        self._target.remove(ser)
+                self.last = data.get('name')
+
+        if data.get('type') == 'midi' and not data.get('context').get('notes'):
+            if self.last == 'led-pattern-4' or self.last == 'bridge':
+                signal = b'\x10'
+
+        if signal:
+            for ser in self._targets:
+                try:
+                    ser.write(signal)
+                    ser.flush()
+                except OSError:
+                    self._targets.remove(ser)
+
